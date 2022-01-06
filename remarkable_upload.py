d0dc7d6cc0bf1238b067d2df5008d3f0aac60ef6
Alexander Schlögl <Alexander Schlgl>
2022-01-06 22:17:25 +0100

    Add 'parallel' upload of multiple files

--- remarkable_upload.py
+++ remarkable_upload.py
@@ -2,15 +2,19 @@
 
 import click
 import requests
-import parallel
+from joblib import Parallel, delayed
 from typing import List
 
 URL = "http://10.11.99.1/upload"
 
 
 @click.command()
-@click.argument("path", type=click.Path(readable=True))
-def main(path):
+@click.argument("paths", nargs=-1, type=click.Path(readable=True))
+def main(paths: List[str]):
+    Parallel()(delayed(upload_path)(path) for path in paths)
+
+
+def upload_path(path: str):
     files = {"file": open(path, "rb")}
     response = requests.post(URL, files=files)
     assert response.ok
